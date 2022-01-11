531959b1bb9eb6e2377beaa53d3dbbc1a1f387c2
Neha <codewithneha2020@gmail.com>
2022-01-11 21:49:05 +0530

    Unary Operators

---
+++ src/Day_1/Unary_Operators.java
@@ -0,0 +1,16 @@
+package Day_1;
+
+public class Unary_Operators {
+ 
+	public static void main(String[] args) {
+		int a = 10;
+		System.out.println("a++ = " + (a++));//11 a = a+1
+		System.out.println("a-- = " + (a--));//10 a = a-1
+		System.out.println(a);
+	}
+}
+
+
+// Unary Operators:- These operators require only one operand to trigger computation.
+// 1. Increment operator:- a++
+// 2. Decrement Operator:- a--
