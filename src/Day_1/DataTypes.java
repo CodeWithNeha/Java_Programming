69abee87bdcebfe28e818d3a52776cf489eafcbb
Neha <codewithneha2020@gmail.com>
2022-01-16 18:45:04 +0530

    DataTypes

---
+++ src/Day_1/DataTypes.java
@@ -0,0 +1,49 @@
+package Day_1;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// DataTypes:- Data Types are used to define types of variables. Data types define right way you use storage in the
+		// programs you write. Example:- int a;
+        
+		// 1. char:- A char is a datatype that can store a single character. Use a single quote when declaring a char.
+		// Example :- char grade = 'A';
+		char grade = 'b';
+		System.out.println(grade);
+		
+		
+		// 2. string:- A string is a sequence of characters. Use a double quotation while declaring a string.
+		// example :- String name= "Sahib";
+		
+		String name = "Oscar";
+		System.out.println(name);
+		
+		// 3. int:- An int is an integer type that has the range -2.14 billion to 2.4 billion. Int is usually the default
+		// choice for an integer unless there is a specific reason to use a different int type.
+		// Example :- int order = 10;
+		
+		int a = 10;
+		System.out.println(a);
+		
+		// 4. float:- A float is also a numeric value that can hold decimals. It is used when programmers need to save memory.
+		// Example:- float a = 3.1444f;
+		 float b = 3.1444f;
+		 System.out.println(b);
+		
+		 // 5. double:- A double is a numeric value. This type is the default choice for decimal values.
+		 // Example:- double money = 15.12;
+		 
+		 double money = 15.12;
+		 System.out.println(money);
+		 
+		 // 6. boolean:- A boolean can store two values: true or false. It is usually used to check a condition of another
+		 // variables or "state".
+		 // Example:- boolean status = true;
+		 
+		 boolean status =  true;
+		 boolean status2 = false;
+		 System.out.println(status);
+		 System.out.println(status2);
+	}
+
+}
