2c76494f5a460a53be8f4cf1956be9121bdd71fc
Neha <codewithneha2020@gmail.com>
2022-01-18 21:55:52 +0530

    Assignment 1

---
+++ src/Day_1/Assignment1.java
@@ -0,0 +1,20 @@
+package Day_1;
+
+import java.util.Scanner;
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		Scanner oscar = new Scanner(System.in);
+		int a = oscar.nextInt();
+		int b = oscar.nextInt();
+		int c = oscar.nextInt();
+		int d = oscar.nextInt();
+		int e = oscar.nextInt();
+		int total = a+b+c+d+e;
+		System.out.println(total);
+		
+
+	}
+
+}
---
+++ src/Day_1/Escape.java
@@ -0,0 +1,13 @@
+package Day_1;
+
+public class Escape {
+
+	public static void main(String[] args) {
+		System.out.print("Sachman \n Singh Sachman \t Singh Sachman \' Singh");
+//		System.out.print("Sachman \t Singh");
+//		System.out.print("Sachman \' Singh");
+//		System.out.print("Sachman \" Singh");
+
+	}
+
+}
