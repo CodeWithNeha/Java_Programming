e4360bc0035e7ab7435f666628666794ea905536
Neha <codewithneha2020@gmail.com>
2022-01-19 21:39:40 +0530

    Errors and Control Structure

---
+++ src/Day_1/Control_Structure.java
@@ -0,0 +1,10 @@
+package Day_1;
+
+public class Control_Structure {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Day_1/RunTimeError.java
@@ -0,0 +1,28 @@
+package Day_1;
+
+public class RunTimeError {
+
+	public static void main(String[] args) {
+	// 1. Syntax Error;-
+//		int a = 50:
+//		System.out.println(a);
+		
+		//2. semantic Error
+//		int x = 10,y = 20,z;
+//		x*y=z;
+		
+		//3. Data Type Errors:-
+//		float x;
+//		char a = 'a',b = 'b';
+//		x = a;
+//		System.out.println(x);
+		
+		//4. Run Time Error
+//		int a = 30/0;
+//		System.out.println(a);
+		
+		
+
+	}
+
+}
