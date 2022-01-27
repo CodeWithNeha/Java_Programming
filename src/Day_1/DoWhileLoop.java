cfe58e570097ec2fe465274a0d166e09ad1f1ba3
Neha <codewithneha2020@gmail.com>
2022-01-27 21:55:30 +0530

    Do While Loop and Sachman's choice

---
+++ src/Day_1/DoWhileLoop.java
@@ -0,0 +1,26 @@
+package Day_1;
+
+public class DoWhileLoop {
+
+	public static void main(String[] args) {
+		// Do While loop :- Exit Control Loop
+		//  1. initialization
+		//  2. test condition
+		//  3. inc/dec
+		//  initialization
+		//  do
+		//  {
+		// 		statement-block;
+		//       inc/dec;
+		//  }
+		//  while(test condition);
+		int i = 1;   // initialization
+		do {
+			System.out.println(i);
+			i++;    // inc/dec
+		}
+		while(i<10);     //test condition
+		
+	}
+
+}
---
+++ src/Day_1/SachmanChoice.java
@@ -0,0 +1,15 @@
+package Day_1;
+
+public class SachmanChoice {
+
+	public static void main(String[] args) {
+		
+		int i = 1;
+		while(i<=10) {
+			System.out.println(i+" % " + 2 + " = " + i%2);
+			i++;
+		}
+
+	}
+
+}
